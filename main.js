127b9bd2d0383c121b2c3b362ed36b7854dea86a
Cem Ekiz <ekizcem.pro@gmail.com>
2022-08-25 17:59:21 +0200

    feat: mobile nav and desktop nav

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+'use strict';
 import './sass/main.scss';
+
+// Variables
+const primaryHeader = document.querySelector('.primary-header');
+const navBtnToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+// Accessible Mobile Menu
+navBtnToggle.addEventListener('click', () => {
+	if (primaryNav.hasAttribute('data-visible')) {
+		navBtnToggle.setAttribute('aria-expanded', false);
+	} else {
+		navBtnToggle.setAttribute('aria-expanded', true);
+	}
+
+	primaryNav.toggleAttribute('data-visible');
+	primaryHeader.toggleAttribute('data-overlay');
+});
